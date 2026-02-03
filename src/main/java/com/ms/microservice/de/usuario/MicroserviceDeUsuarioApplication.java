9870aa67a8b8e0da27a720a5b5be6590e2654fdd
victornext <victorbrs517@gmail.com>
2026-02-03 16:47:43 -0300

    Base

---
+++ src/main/java/com/ms/microservice/de/usuario/MicroserviceDeUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.ms.microservice.de.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDeUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDeUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/microservice/de/usuario/MicroserviceDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.microservice.de.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDeUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
